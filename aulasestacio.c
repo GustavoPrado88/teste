ceaa91f61bc1fdd910878dc1f6824cd4f954e3bc
GustavoPrado88 <gustavoinvest2022@gmail.com>
2025-09-01 20:51:29 -0300

    Update aulasestacio.c

--- aulasestacio.c
+++ aulasestacio.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-void main(){
-    printf("teste");
-}
+ int main(){
+
+    int idade;
+    float altura;
+    char opcao;
+    char nome [20];
+    
+    //teste 2
+
+    printf("digite sua idade: ");
+    scanf("%i", &idade);
+    printf("a idade é: %i\n", idade);
+
+    printf("A sua altura é: ");
+    scanf("%f", &altura);
+    printf("A altura é: %f\n", altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    printf("Seu nome é: %s", nome);
+
+
+
+ }
